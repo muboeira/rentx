04772866491a1899d0e2de58c880079240a85518
Murilo Baptista <murilo.baptista@ateliware.com>
2022-07-09 10:45:06 -0300

    first commit

---
+++ src/database/index.ts
@@ -0,0 +1,22 @@
+import { DataSource } from "typeorm";
+
+import { Category } from "../modules/cars/entities/Category";
+import { Specification } from "../modules/cars/entities/Specification";
+
+export const dataSource = new DataSource({
+  type: "postgres",
+  host: "database",
+  port: 5432,
+  username: "docker",
+  password: "1234",
+  database: "rentx",
+  entities: [Category, Specification],
+  migrations: ["src/database/migrations/*.ts"],
+});
+
+dataSource
+  .initialize()
+  .then(async () => {
+    console.log("Initializing the database...");
+  })
+  .catch((err) => console.log(err));
---
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -0,0 +1,32 @@
+import { Repository } from "typeorm";
+
+import { dataSource } from "../../../../database";
+import { Specification } from "../../entities/Specification";
+import {
+  ICreateSpecificationDTO,
+  ISpecificationsRepository,
+} from "../ISpecificationsRepository";
+
+class SpecificationsRepository implements ISpecificationsRepository {
+  private repository: Repository<Specification>;
+
+  constructor() {
+    this.repository = dataSource.getRepository(Specification);
+  }
+
+  async create({ name, description }: ICreateSpecificationDTO): Promise<void> {
+    const specification = this.repository.create({
+      name,
+      description,
+    });
+
+    await this.repository.save(specification);
+  }
+
+  async findByName(name: string): Promise<Specification> {
+    const specification = await this.repository.findOneBy({ name });
+    return specification;
+  }
+}
+
+export { SpecificationsRepository };
